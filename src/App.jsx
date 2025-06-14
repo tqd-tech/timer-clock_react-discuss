426a4ba430d6aa60dbea54bcdb899e5406a002cf
Tuan <tuanlenguyen612@gmail.com>
2025-06-14 22:31:32 +0700

    Initial commit after migration from Clock

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Stopwatch from './Stopwatch';
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="heading">STOP WATCH</h1>
+      <Stopwatch />
+    </div>
+  );
+}
+
+export default App;
